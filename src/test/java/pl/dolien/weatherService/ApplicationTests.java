79be497a87fcc38a9470d4f59eda1afc98e4368a
dolienn <thedolien@gmail.com>
2024-08-28 16:03:44 +0200

    added git-flow support

---
+++ src/test/java/pl/dolien/weatherService/ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.dolien.weatherService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
