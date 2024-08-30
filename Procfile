e42b7b02cceace677953c3eeaccaa96bdcdddde8
dolienn <thedolien@gmail.com>
2024-08-30 21:46:40 +0200

    Added Environments in Angular and Heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java -Dserver.port=8080 -jar target/Worldwide-Windsurfer-s-Weather-Service-0.0.1-SNAPSHOT.jar
--- windsurfers-weather-service-ui/src/app/service/weather-forecast/weather-forecast.service.ts
+++ windsurfers-weather-service-ui/src/app/service/weather-forecast/weather-forecast.service.ts
@@ -1,13 +1,13 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { map, Observable } from 'rxjs';
+import { environment } from '../../../environments/environment.development';
 
 @Injectable({
   providedIn: 'root',
 })
 export class WeatherForecastService {
-  private readonly wfUrl =
-    'http://localhost:8080/api/windsurfing/best-location';
+  private readonly wfUrl = `${environment.apiUrl}/best-location`;
 
   constructor(private httpClient: HttpClient) {}
 
