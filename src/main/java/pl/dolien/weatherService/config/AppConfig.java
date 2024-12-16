3fcafa8e7756c0ce29e02d3baedc1347d94113ae
dolienn <thedolien@gmail.com>
2024-12-17 00:39:13 +0100

    Allow specific origins in CORS configuration
    Updated the CORS configuration to include Heroku URL and multiple localhost origins for development. This ensures proper functionality across specified environments while maintaining security settings.

--- src/main/java/pl/dolien/weatherService/config/AppConfig.java
+++ src/main/java/pl/dolien/weatherService/config/AppConfig.java
@@ -30,7 +30,8 @@ public class AppConfig {
         final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         final CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.setAllowedOrigins(List.of());
+        config.setAllowedOrigins(List.of(herokuUrl, "http://localhost:4200", "http://localhost:8089",
+                "http://localhost"));
         config.setAllowedHeaders(Arrays.asList(
                 ORIGIN,
                 CONTENT_TYPE,
