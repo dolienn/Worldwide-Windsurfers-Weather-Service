387f0e57d900707e3355a978b52e01a7c02acd6e
dolienn <thedolien@gmail.com>
2024-12-17 00:29:21 +0100

    Update allowed origins for CORS configuration
    Added "http://localhost:80" to the list of allowed origins in the CORS setup. This ensures compatibility with services running on the specified port, improving local development and testing flexibility.

--- src/main/java/pl/dolien/weatherService/config/AppConfig.java
+++ src/main/java/pl/dolien/weatherService/config/AppConfig.java
@@ -30,7 +30,8 @@ public class AppConfig {
         final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         final CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.setAllowedOrigins(List.of(herokuUrl, "http://localhost:4200", "http://localhost:8089", "http://localhost"));
+        config.setAllowedOrigins(List.of(herokuUrl, "http://localhost:4200", "http://localhost:8089",
+                "http://localhost:80"));
         config.setAllowedHeaders(Arrays.asList(
                 ORIGIN,
                 CONTENT_TYPE,
